0256529146fbb2bda03c330f2b43339789e02cd0
vishalsinghIT <vishalprasadsingh@gmail.com>
2024-09-25 19:22:43 +0530

    first cmmit

--- Basic_JAVA/EvenOdd.java
+++ Basic_JAVA/EvenOdd.java
@@ -1,3 +1,17 @@
+import java.util.*;
 public class EvenOdd {
-    
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("Enter the value of n");
+        n=sc.nextInt();
+        if(n%2==2)
+        {
+            System.out.println("Number is Even ");
+        }else{
+            System.out.println("Number is odd");
+        }
+        sc.close();
+    }
 }
