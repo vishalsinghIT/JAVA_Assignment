13c1c0a645719635c55965f56a6450f4dd0bfe20
vishalsinghIT <vishalprasadsingh@gmail.com>
2024-09-29 10:47:31 +0530

    first commit

---
+++ Conditional_JAVA/Multiplication.java
@@ -0,0 +1,18 @@
+package Conditional_JAVA;
+
+import java.util.*;
+public class Multiplication {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("Enter the value of n");
+        n=sc.nextInt();
+        for(int i=1; i<=20; i++)
+        {
+            int mul= n*i;
+            System.out.println(n+ " x " +i+ " = " +mul);
+            53
+        }
+    }
+}
---
+++ Conditional_JAVA/SumOfN.java
@@ -0,0 +1,18 @@
+package Conditional_JAVA;
+
+import java.util.*;
+public class SumOfN {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("Enter the value of n");
+        n=sc.nextInt();
+        int sum=0;
+        for(int i=1; i<=n; i++)
+        {
+            sum=sum+i;
+            System.out.println("Sum is " +sum);
+        }
+    }
+}
